2acaeeeb0a437d6719d1087f9bbc07fc923d56c1
Patrick Biffle <pbiffle@gmail.com>
2019-02-19 10:49:42 -0700

    changed ticker orderby to asc

--- queries/stocks.js
+++ queries/stocks.js
@@ -2,7 +2,7 @@ const db = require('../database-connection')
 
 module.exports = {
   getStocksByUser (userId) {
-    return db('stockmoji_snapshots').where('user_id', userId).orderBy('stock_ticker', 'desc').orderBy('date_time', 'desc').returning('*')
+    return db('stockmoji_snapshots').where('user_id', userId).orderBy('stock_ticker', 'asc').orderBy('date_time', 'desc').returning('*')
   },
   addStock (stock) {
     return db('stockmoji_snapshots').insert(stock)
